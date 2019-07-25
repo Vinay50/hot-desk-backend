13e531ee49d9aecc6fde7f759c95d4966c0f3d2f
Vieenay Siingh <vieenaysiingh@gmail.com>
2019-07-25 13:58:10 +0530

    added sign in and sign up endpoints

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+# app/controllers/users_controller.rb
+class UsersController < ApplicationController
+  skip_before_action :authenticate_request, only: :create
+
+  # POST /signup
+  # return authenticated token upon signup
+  def create
+    user = User.create!(user_params)
+    auth_token = AuthenticateUser.new(user.email, user.password).call
+    response = { message: Message.account_created, auth_token: auth_token }
+    # json_response(response, :created)
+    render json: response, status: :created
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password)
+  end
+
+end
